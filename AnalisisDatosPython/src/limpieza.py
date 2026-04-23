0094b9d0d19e9f69fe333a2392fe97d1fbb62635
PaolaArismendy <paolaarismendy431@gmail.com>
2026-04-22 21:44:38 -0500

    Se finaliza la creación de la funciòn limpieza

--- AnalisisDatosPython/src/limpieza.py
+++ AnalisisDatosPython/src/limpieza.py
@@ -1,15 +1,27 @@
 import pandas as pd
 
 def cargar_datos(ruta):
-	"""Carga un archivo CSV en un DataFrame"""
-	return pd.read_csv(ruta)
+    """Carga un archivo CSV en un DataFrame"""
+    return pd.read_csv(ruta)
 
 def manejar_valores_nulos(df):
-	"""Elimina filas con valores nulos"""
-	return df.dropna()
+    """Elimina filas con valores nulos"""
+    return df.dropna()
 
 def estandarizar_texto(df, columnas):
     """Convierte texto a minúsculas y elimina espacios"""
     for col in columnas:
         df[col] = df[col].str.lower().str.strip()
     return df
+
+def validar_notas(df):
+    """Mantiene solo notas válidas entre 0 y 5"""
+    df = df[df["nota"] >= 0]
+    df = df[df["nota"] <= 5]
+    return df
+
+def eliminar_duplicados(df, columna_id):
+    """Elimina registros duplicados"""
+    return df.drop_duplicates(subset=columna_id)
+
+
