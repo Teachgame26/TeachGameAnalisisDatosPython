bf5e5746f7863f58ce525217d17f77fd9fad0955
Teachgame26 <teachgamec@gmail.com>
2026-05-29 21:29:45 -0500

    Agregar endpoint debug

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pathlib import Path
+import pandas as pd
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "mensaje": "API funcionando"
+    }
+
+@app.get("/api/interno/datos-limpios")
+def entregar_headless():
+    df = pd.DataFrame([{"id": 1, "valor": 50}])
+
+    return {
+        "status": "ok",
+        "payload": df.to_dict(orient="records")
+    }
 
 @app.get("/debug")
 def debug():
+
     base = Path(__file__).parent
 
     return {
-        "main_py": str(base),
-        "existe_data": (base / "data").exists(),
-        "existe_reports": (base / "data" / "reports").exists(),
-        "existe_png": (base / "data" / "reports" / "reporte_impresion.png").exists()
+        "base": str(base),
+        "data": (base / "data").exists(),
+        "reports": (base / "data" / "reports").exists(),
+        "png": (base / "data" / "reports" / "reporte_impresion.png").exists()
     }
 
+@app.get("/imagen")
+def obtener_imagen():
+
+    ruta = Path(__file__).parent / "data" / "reports" / "reporte_impresion.png"
+
+    return FileResponse(
+        str(ruta),
+        media_type="image/png"
+    )
 
 
     
