f5a9e15702ce84394a482212e95aac85cde98401
Teachgame26 <teachgamec@gmail.com>
2026-05-29 19:10:45 -0500

    Agrega endpoint raiz

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import pandas as pd
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {
+        "mensaje": "API funcionando"
+    }
+
 @app.get("/api/interno/datos-limpios")
 def entregar_headless():
     df = pd.DataFrame([{"id": 1, "valor": 50}])
+
     # Retorno estricto orientado a filas (headless JSON payload)
-    return {"status": "ok", "payload": df.to_dict(orient="records")}
+    return {
+        "status": "ok",
+        "payload": df.to_dict(orient="records")
+    }
+
+
+
+    
